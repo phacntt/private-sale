ce4d07db597652deb212432f55239f6df6e2cb3d
zuno90 <pnlan1406@gmail.com>
2022-12-18 02:03:14 +0700

    zuno test deploy

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -8,40 +8,53 @@ import { CreateUserDto } from "../dtos/user.dto";
 const prisma = new PrismaClient();
 
 class UserService {
-    public async getUsers() {    
-        const users = await prisma.user.findMany()
-        if (!users) throw new HttpException(409, "You're not user");
-    
-        return users;
-    }
+  public async getUsers() {
+    const users = await prisma.user.findMany();
+    if (!users) throw new HttpException(409, "You're not user");
 
-    public async findUserById(userId: number) {
-        if (isEmpty(userId)) throw new HttpException(409, "You not permision");
-    
-        const findUser = await prisma.user.findUnique({where: {id: userId}, include: {posts: true}})
-        if (!findUser) throw new HttpException(409, "You're not user");
-    
-        return findUser;
-    }
+    return users;
+  }
 
-    public async findUserByEmail(email: string) {
-        if (isEmpty(email)) throw new HttpException(409, "You're email not found!! Please check again")
-    
-        const findUser = await prisma.user.findUnique({where: {email}})
-    
-        return findUser;
-    }
+  public async findUserById(userId: number) {
+    if (isEmpty(userId)) throw new HttpException(409, "You not permision");
 
-    public async createUser(userData: CreateUserDto) {
-        if (isEmpty(userData)) throw new HttpException(400, "You're not userData");
+    const findUser = await prisma.user.findUnique({
+      where: { id: userId },
+      include: { posts: true },
+    });
+    if (!findUser) throw new HttpException(409, "You're not user");
 
-        const findUser = await prisma.user.findUnique({where: {email: userData.email}})
-        if (findUser) throw new HttpException(400, `You're email ${userData.email} already exists`);
+    return findUser;
+  }
 
-        const createUserData = await prisma.user.create({data: userData})
+  public async findUserByEmail(email: string) {
+    if (isEmpty(email))
+      throw new HttpException(
+        409,
+        "You're email not found!! Please check again"
+      );
 
-        return createUserData;
-    }
+    const findUser = await prisma.user.findUnique({ where: { email } });
+
+    return findUser;
+  }
+
+  public async createUser(userData: CreateUserDto) {
+    if (isEmpty(userData)) throw new HttpException(400, "You're not userData");
+
+    const findUser = await prisma.user.findUnique({
+      where: { email: userData.email },
+    });
+    if (findUser)
+      throw new HttpException(
+        400,
+        `You're email ${userData.email} already exists`
+      );
+
+    const createUserData = await prisma.user.create({ data: userData });
+
+    return createUserData;
+  }
 }
 
-export default UserService
+export default UserService;
