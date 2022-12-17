9020695a3a0c327361865e32903867dacc2ee7e2
Tran Anh Pha <trananhpha2001@gmail.com>
2022-12-17 15:31:12 +0700

    add gitignore

--- dist/src/config/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PORT = exports.NODE_ENV = void 0;
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)({ path: `.env.${process.env.NODE_ENV || 'development'}` });
-_a = process.env, exports.NODE_ENV = _a.NODE_ENV, exports.PORT = _a.PORT;
-//# sourceMappingURL=index.js.map
