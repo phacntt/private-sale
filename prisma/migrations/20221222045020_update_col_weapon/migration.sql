4a00a69ec518a966e40118a29c412941be12b497
Tran Anh Pha <trananhpha2001@gmail.com>
2022-12-22 11:50:59 +0700

    update test deploy

---
+++ prisma/migrations/20221222045020_update_col_weapon/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `phone` on the `Weapon` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Weapon" DROP COLUMN "phone";
