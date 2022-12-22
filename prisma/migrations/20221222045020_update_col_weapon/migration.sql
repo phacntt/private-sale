ade7796d6bcf5543a2fbb2fccca4ffb2d917e923
Tran Anh Pha <trananhpha2001@gmail.com>
2022-12-22 14:48:23 +0700

    update schema test

--- prisma/migrations/20221222045020_update_col_weapon/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `phone` on the `Weapon` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Weapon" DROP COLUMN "phone";
--- prisma/migrations/20221222055733_add_col_weapon/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `critRate` to the `Weapon` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Weapon" ADD COLUMN     "critRate" INTEGER NOT NULL;
